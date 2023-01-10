b68bee96123980c8454560a9df97c12720ec58ec
chubinashvili <chubinashvili@pop-os.localdomain>
2023-01-10 18:13:58 +0400

    added staking

--- src/components/Staking.js
+++ src/components/Staking.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Staking } from "@cubitrix/cubitrix-react-staking-module";
+import { Staking } from "cubitrix-react-staking-module";
 import useConnect from '../hooks/use-connect'
 import STACK_ABI from '../stack.json';
 import WBNB from '../WBNB.json';
