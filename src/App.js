d354e85e1c06c1153ea15b70bbbfaf57eb55ccfa
sergo93@gmail.com <sergo93@gmail.com>
2023-02-20 12:30:48 +0400

    last

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Dashboard from "./components/Dashboard";
 import Trade from "./components/Trade";
 import Loan from "./components/Loan";
 import Referal from "./components/Referal";
-import Stake from "./components/Staking";
+import Staking from "./components/Staking";
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Layouts/Footer/Footer";
 import Extensions from "./components/Extensions";
@@ -102,8 +102,8 @@ function App() {
             <Route path="/" element={<Dashboard />} />
             <Route path="/loan" element={<Trade />} />
             <Route path="/trade" element={<Loan />} />
-            <Route path="/staking" element={<Referal />} />
-            <Route path="/referal" element={<Stake />} />
+            <Route path="/staking" element={<Staking />} />
+            <Route path="/referal" element={<Staking />} />
             <Route path="/extensions" element={<Extensions />} />
             <Route path="/recovery" element={<RecoveryLogin />} />
             <Route path="/verify/:id" element={<VerifyEmail />} />
