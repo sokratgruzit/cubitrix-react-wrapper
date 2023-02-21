bb847ede42283cd39c14bd91770e02f1c625fc0f
sergo93@gmail.com <sergo93@gmail.com>
2023-02-21 17:43:07 +0400

    add stake func

--- src/components/Staking.js
+++ src/components/Staking.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // import axios from '../api/axios';
 // import { useSelector } from "react-redux";
 
@@ -9,8 +9,288 @@ import { useTableParameters } from '../hooks/useTableParameters';
 
 // UI
 import { Staking, Button } from '@cubitrix/cubitrix-react-ui-module';
+import { useConnect } from "@cubitrix/cubitrix-react-connect-module";
+import STACK_ABI from "../abi/stack.json";
+import WBNB from "../abi/WBNB.json";
+import moment from "moment";
 
 const Stake  = () => {
+    const {
+        connect,
+        disconnect,
+        library,
+        account,
+        isActive,
+        walletModal,
+        handleWalletModal,
+    } = useConnect;
+    var web3Obj = library;
+
+    var Router = "0x61d27DFd33718E47FBcFBf31B8e96439D3eccbdD"; // Staking contract Address
+    var tokenAddress = "0xb2343143f814639c9b1f42961C698247171dF34a"; // Staking Token Address
+
+    // const account = useSelector(state => state.connect.account);
+    const [dipositAmount, setDipositAmount] = useState("");
+    const [timeperiod, setTimeperiod] = useState(0);
+    const [timeperiodDate, setTimeperiodDate] = useState(
+        moment().add(30, "days").format("DD/MM/YYYY h:mm A")
+    );
+
+    const [balance, setBalance] = useState(0);
+
+    const [stackContractInfo, setStackContractInfo] = useState({
+        totalStakers: 0,
+        totalStakedToken: 0,
+    });
+    const [stakersInfo, setStakersInfo] = useState({
+        totalStakedTokenUser: 0,
+        totalUnstakedTokenUser: 0,
+        totalClaimedRewardTokenUser: 0,
+        currentStaked: 0,
+        realtimeReward: 0,
+        stakeCount: 0,
+        alreadyExists: false,
+    });
+    const [stakersRecord, setStakersRecord] = useState([]);
+
+    const [isAllowance, setIsAllowance] = useState(false);
+    const [loading, setLoadding] = useState(false);
+
+    const notify = (isError, msg) => {
+        if (isError) {
+           console.log('error')
+        } else {
+            console.log('success')
+        }
+    };
+
+    const checkAllowance = async () => {
+        try {
+            setLoadding(true);
+
+            var tokenContract = new web3Obj.eth.Contract(WBNB, tokenAddress);
+            var decimals = await tokenContract.methods.decimals().call();
+            var getBalance = await tokenContract.methods.balanceOf(account).call();
+
+            var pow = 10 ** decimals;
+            var balanceInEth = getBalance / pow;
+            setBalance(balanceInEth);
+            var allowance = await tokenContract.methods
+                .allowance(account, Router)
+                .call();
+
+            if (allowance <= 2) {
+                setIsAllowance(true);
+            }
+            if (dipositAmount > 0) {
+                var amount = dipositAmount * pow;
+                if (allowance < amount) {
+                    setIsAllowance(true);
+                }
+            }
+            setLoadding(false);
+        } catch (err) {
+            setLoadding(false);
+        }
+    };
+
+    const approve = async () => {
+        setLoadding(true);
+        try {
+            var contract = new web3Obj.eth.Contract(WBNB, tokenAddress);
+
+            var amountIn = 10 ** 69;
+            amountIn = amountIn.toLocaleString("fullwide", { useGrouping: false });
+            //   var amountIn = new web3Obj.utils.BigNumber("10").pow(69);
+
+            await contract.methods
+                .approve(Router, amountIn.toString())
+                .send({ from: account })
+                .then(() => {
+                    setIsAllowance(false);
+                    // checkAllowance("0xaae3d23a76920c9064aefdd571360289fcc80053");
+                    setLoadding(false);
+                });
+        } catch (err) {
+            console.log(err);
+            setLoadding(false);
+            notify(true, err.message);
+        }
+    };
+
+    const stake = async () => {
+        if (isNaN(parseFloat(dipositAmount)) || parseFloat(dipositAmount) <= 0) {
+            notify(true, "Error! please enter amount");
+            return;
+        }
+        await checkAllowance();
+        setLoadding(true);
+        try {
+            var tokenContract = new web3Obj.eth.Contract(WBNB, tokenAddress);
+            const decimals = await tokenContract.methods.decimals().call();
+
+            var contract = new web3Obj.eth.Contract(STACK_ABI, Router);
+
+            var pow = 10 ** decimals;
+            var amountIn = dipositAmount * pow;
+            // var amountInNew = `${new ethers.utils.BigNumber(amountIn.toString())}`;
+            amountIn = amountIn.toLocaleString("fullwide", { useGrouping: false });
+
+            await contract.methods
+                .stake(amountIn.toString(), timeperiod.toString())
+                .send({ from: account })
+                .then((err) => {
+                    getStackerInfo();
+                    setLoadding(false);
+                    notify(false, "Staking process complete.");
+                });
+        } catch (err) {
+            setLoadding(false);
+            notify(true, err.message);
+        }
+    };
+
+    const unstake = async (index) => {
+        setLoadding(true);
+        try {
+            var contract = new web3Obj.eth.Contract(STACK_ABI, Router);
+            await contract.methods
+                .unstake(index.toString())
+                .send({ from: account })
+                .then((result) => {
+                    getStackerInfo();
+                    setLoadding(false);
+                    notify(false, "successfully unstake");
+                    //   withdrawModal();
+                });
+        } catch (err) {
+            setLoadding(false);
+            notify(true, "unstake fail");
+        }
+    };
+
+    const harvest = async (index) => {
+        setLoadding(true);
+        try {
+            var contract = new web3Obj.eth.Contract(STACK_ABI, Router);
+            await contract.methods
+                .harvest(index.toString())
+                .send({ from: account })
+                .then((err) => {
+                    getStackerInfo();
+                    setLoadding(false);
+                    checkAllowance();
+                    notify(false, "Reward successfully harvested");
+                });
+        } catch (err) {
+            console.log(err);
+            setLoadding(false);
+            notify(true, err.message);
+        }
+    };
+
+    const getStackerInfo = async () => {
+        setLoadding(true);
+        try {
+            var tokenContract = new web3Obj.eth.Contract(WBNB, tokenAddress);
+            var decimals = await tokenContract.methods.decimals().call();
+            var getBalance = await tokenContract.methods
+                .balanceOf(account.toString())
+                .call();
+            var pow = 10 ** decimals;
+            var balanceInEth = getBalance / pow;
+            console.log(getBalance);
+            console.log(balanceInEth);
+            setBalance(balanceInEth);
+
+            var contract = new web3Obj.eth.Contract(STACK_ABI, Router);
+            var totalStakedToken = await contract.methods.totalStakedToken
+                .call()
+                .call();
+            var totalStakers = await contract.methods.totalStakers.call().call();
+            var realtimeReward = await contract.methods
+                .realtimeReward(account)
+                .call();
+            var Stakers = await contract.methods.Stakers(account).call();
+
+            var totalStakedTokenUser = Stakers.totalStakedTokenUser / pow;
+            var totalUnstakedTokenUser = Stakers.totalUnstakedTokenUser / pow;
+            var currentStaked = totalStakedTokenUser - totalUnstakedTokenUser;
+            totalStakedToken = totalStakedToken / pow;
+
+            Stakers.totalStakedTokenUser = totalStakedTokenUser;
+            Stakers.totalUnstakedTokenUser = totalUnstakedTokenUser;
+            Stakers.currentStaked = currentStaked;
+            Stakers.realtimeReward = realtimeReward / pow;
+            Stakers.totalClaimedRewardTokenUser =
+                Stakers.totalClaimedRewardTokenUser / pow;
+            var stakersRecord = [];
+            for (var i = 0; i < parseInt(Stakers.stakeCount); i++) {
+                var stakersRecordData = await contract.methods
+                    .stakersRecord(account, i)
+                    .call();
+
+                var realtimeRewardPerBlock = await contract.methods
+                    .realtimeRewardPerBlock(account, i.toString())
+                    .call();
+
+                stakersRecordData.realtimeRewardPerBlock =
+                    realtimeRewardPerBlock[0] / pow;
+
+                stakersRecordData.unstaketime = moment
+                    .unix(stakersRecordData.unstaketime)
+                    .format("DD/MM/YYYY h:mm A");
+                stakersRecordData.staketime = moment
+                    .unix(stakersRecordData.staketime)
+                    .format("DD/MM/YYYY h:mm A");
+                stakersRecord.push(stakersRecordData);
+            }
+            setStakersInfo(Stakers);
+            setStakersRecord(stakersRecord);
+            setStackContractInfo({
+                totalStakers: totalStakers,
+                totalStakedToken: totalStakedToken,
+            });
+            setLoadding(false);
+        } catch (err) {
+            // console.log(err);
+            setLoadding(false);
+            setStakersInfo({
+                totalStakedTokenUser: 0,
+                totalUnstakedTokenUser: 0,
+                totalClaimedRewardTokenUser: 0,
+                currentStaked: 0,
+                realtimeReward: 0,
+                stakeCount: 0,
+                alreadyExists: false,
+            });
+            setStackContractInfo({
+                totalStakers: 0,
+                totalStakedToken: 0,
+            });
+            setStakersRecord([]);
+            setBalance(0);
+        }
+    };
+
+    const setMaxWithdrawal = async () => {
+        var tokenContract = new web3Obj.eth.Contract(WBNB, tokenAddress);
+        var decimals = await tokenContract.methods.decimals().call();
+        var getBalance = await tokenContract.methods
+            .balanceOf(account.toString())
+            .call();
+        var pow = 10 ** decimals;
+        var balanceInEth = getBalance / pow;
+        setDipositAmount(balanceInEth.toFixed(5));
+        // setWithdrawAmount(userInfo.staked);
+    };
+
+    useEffect(() => {
+        if (isActive) {
+            checkAllowance();
+            getStackerInfo();
+        }
+    }, [isActive, account]);
     // const account = useSelector(state => state.connect.account);
     // async function stake() {
     //     await axios.post("/api/stake", {
