86e8b7ab0ecfab412ecd0a145a72e0619f7bcd75
salo <chkhartishvili202@gmail.com>
2023-03-10 16:58:31 +0400

    update

--- src/components/Landing/Footer/Footer.js
+++ src/components/Landing/Footer/Footer.js
@@ -1,5 +1,5 @@
+import React, { useState } from "react";
 
-// svgs
 import Logo from "../../../assets/svg/Logo";
 import FaceBook from "../../../assets/svg/FaceBook";
 import Linkedin from "../../../assets/svg/Linkedin";
@@ -8,33 +8,45 @@ import GitHub from "../../../assets/svg/GitHub";
 
 import styles from "./Footer.module.css";
 
-const Footer =({ email, setEmail, handleSubmit }) => {
+const Footer =(props) => {
+  const [inputValue, setInputValue] = useState("");
+
   const handleChange = (e) => {
-    setEmail(e.target.value);
+    const value = e.target.value;
+    setInputValue(value);
+    props.handleInput(value);
   };
 
-  return (
-      <div className={styles.footer}>
-      <div className={styles.logo}>
-        <div>
-          <Logo />
+  const handleButtonClick = () => {
+    props.handleButtonClick("footer Button clicked");
+  };
+
+    return (
+        <div className={styles.footerContent}>
+        <div className={styles.footerLogo}>
+          {/* <div className={styles.footerWrap}> */}
+          <div>
+            <Logo />
+          </div>
+          <span>COMPLAND</span>
         </div>
-        <span>COMPLAND</span>
+        <div className={styles.footerForm}>
+              {/* <form> */}
+              <input type="text" name="" placeholder="Enter email adress" value={inputValue} onChange={handleChange}/>
+              <button type="submit" onClick={handleButtonClick} >Subscribe News</button>
+              {/* </form> */}
+        {/* </div> */}
+        </div>
+        <div className={styles.footerIcons}>
+            <div className={styles.ftIcon}><FaceBook/></div>
+            <div className={styles.ftIcon}><Twitter/></div>
+            <div className={styles.ftIcon}><Linkedin/></div>
+            <div className={styles.ftIconBorder}><GitHub/></div>
+        </div>
       </div>
-      <div className={styles.form}>
-            <input type="text" name="email" placeholder="Enter email adress" value={email} onChange={handleChange}/>
-            <button type="submit" onClick={handleSubmit} >Subscribe News</button>
-      </div>
-      <div className={styles.icons}>
-          <div className={styles.border}><FaceBook/></div>
-          <div className={styles.border}><Twitter/></div>
-          <div className={styles.border}><Linkedin/></div>
-          <div className={styles.noBorder}><GitHub/></div>
-      </div>
-    </div>
 
 
-  )
+    )
 };
 
 export default Footer;
