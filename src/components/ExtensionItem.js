808b74524c12d9553b27855cd33fb2500e1dde6a
Zura Chubinashvili <chubinashvili2003@gmail.com>
2023-04-06 18:41:41 +0400

    feat: add extensions

--- src/components/ExtensionItem.js
+++ src/components/ExtensionItem.js
@@ -4,6 +4,8 @@ import { useSelector, useDispatch } from "react-redux";
 
 import axios from "../api/axios";
 
+import { useParams } from "react-router-dom";
+
 import { InnerExtensions as ExtensionItemUI } from "@cubitrix/cubitrix-react-ui-module";
 import { StakingIcon } from "../assets/svg";
 
@@ -11,9 +13,19 @@ const ExtensionItem = () => {
   const account = useSelector((state) => state.connect.account);
   const dispatch = useDispatch();
   const { activeExtensions } = useSelector((state) => state.extensions);
+  const appState = useSelector((state) => state.appState);
+  const emailVerified = appState.emailVerified;
+  const isActive = appState.userData?.system?.[0]?.active;
+  const { id } = useParams();
   // console.log((activeExtensions));
 
   function testChangeExtension(title, boolean) {
+    if (title === "loan") {
+      if (!isActive || !emailVerified) return;
+    }
+    if (title === "staking" || title === "referral") {
+      if (!emailVerified) return;
+    }
     axios
       .post("/api/accounts/manage_extensions", {
         address: account,
@@ -35,51 +47,65 @@ const ExtensionItem = () => {
     {
       icon: <StakingIcon className={"other-extensions-card-icon"} />,
       title: "Trade",
+      value: "trade",
       description:
         "Crust pencil novel colours drift unfamed, oft line balls instructed sociis.",
       hash: "0x74a81F84268744a40FEBc48f8b812a1f188D80C3",
-      handleSwitch: (title, boolean) => testChangeExtension(title.toLowerCase(), boolean),
+      handleSwitch: (title, value) =>
+        testChangeExtension(title.toLowerCase(), value),
       active: activeExtensions.trade === "true" ? true : false,
+      disabled: false,
     },
     {
       icon: <StakingIcon className={"other-extensions-card-icon"} />,
       title: "Staking",
+      value: "staking",
       description:
         "Crust pencil novel colours drift unfamed, oft line balls instructed sociis.",
       hash: "0x74a81F84268744a40FEBc48f8b812a1f188D80C3",
-      handleSwitch: (title, boolean) => testChangeExtension(title.toLowerCase(), boolean),
+      handleSwitch: (title, value) =>
+        testChangeExtension(title.toLowerCase(), value),
       active: activeExtensions.staking === "true" ? true : false,
+      disabled: !emailVerified,
     },
     {
       icon: <StakingIcon className={"other-extensions-card-icon"} />,
       title: "Loan",
+      value: "loan",
       description:
         "Crust pencil novel colours drift unfamed, oft line balls instructed sociis.",
       hash: "0x74a81F84268744a40FEBc48f8b812a1f188D80C3",
-      handleSwitch: (title, boolean) => testChangeExtension(title.toLowerCase(), boolean),
+      handleSwitch: (title, value) =>
+        testChangeExtension(title.toLowerCase(), value),
       active: activeExtensions.loan === "true" ? true : false,
+      disabled: !(isActive && emailVerified),
     },
     {
       icon: <StakingIcon className={"other-extensions-card-icon"} />,
       title: "Referral",
+      value: "referral",
       description:
         "Crust pencil novel colours drift unfamed, oft line balls instructed sociis.",
       hash: "0x74a81F84268744a40FEBc48f8b812a1f188D80C3",
-      handleSwitch: (title, boolean) => testChangeExtension(title.toLowerCase(), boolean),
+      handleSwitch: (title, value) =>
+        testChangeExtension(title.toLowerCase(), value),
       active: activeExtensions.referral === "true" ? true : false,
+      disabled: !emailVerified,
     },
     {
       icon: <StakingIcon className={"other-extensions-card-icon"} />,
       title: "Notifications",
+      value: "notify",
       description:
         "Crust pencil novel colours drift unfamed, oft line balls instructed sociis.",
       hash: "0x74a81F84268744a40FEBc48f8b812a1f188D80C3",
-      handleSwitch: (title, boolean) => testChangeExtension("notify", boolean),
+      handleSwitch: (title, value) => testChangeExtension("notify", value),
       active: activeExtensions.notify === "true" ? true : false,
+      disabled: !(isActive && emailVerified),
     },
   ];
 
-  return <ExtensionItemUI extensionsCardsData={extensionsCardsData} />;
+  return <ExtensionItemUI extensionsCardsData={extensionsCardsData} id={id} />;
 };
 
 export default ExtensionItem;
--- src/components/Extensions.js
+++ src/components/Extensions.js
@@ -11,14 +11,21 @@ const Extensions = () => {
   const account = useSelector((state) => state.connect.account);
   const dispatch = useDispatch();
   const { activeExtensions } = useSelector((state) => state.extensions);
-  // console.log((activeExtensions));
+  const appState = useSelector((state) => state.appState);
+  const emailVerified = appState.emailVerified;
+  const isActive = appState.userData?.system?.[0]?.active;
 
-  function testChangeExtension(title, boolean) {
-
+  function testChangeExtension(title, value) {
+    if (title === "loan") {
+      if (!isActive || !emailVerified) return;
+    }
+    if (title === "staking" || title === "referral") {
+      if (!emailVerified) return;
+    }
     axios
       .post("/api/accounts/manage_extensions", {
         address: account,
-        extensions: { [title]: boolean === true ? "true" : "false" },
+        extensions: { [title]: value === true ? "true" : "false" },
       })
       .then((res) => {
         console.log(res.data);
@@ -36,47 +43,61 @@ const Extensions = () => {
     {
       icon: <StakingIcon className={"other-extensions-card-icon"} />,
       title: "Trade",
+      value: "trade",
       description:
         "Crust pencil novel colours drift unfamed, oft line balls instructed sociis.",
       hash: "0x74a81F84268744a40FEBc48f8b812a1f188D80C3",
-      handleSwitch: (title, boolean) => testChangeExtension(title, boolean),
+      handleSwitch: (title, value) =>
+        testChangeExtension(title.toLowerCase(), value),
       active: activeExtensions.trade === "true" ? true : false,
+      disabled: false,
     },
     {
       icon: <StakingIcon className={"other-extensions-card-icon"} />,
       title: "Staking",
+      value: "staking",
       description:
         "Crust pencil novel colours drift unfamed, oft line balls instructed sociis.",
       hash: "0x74a81F84268744a40FEBc48f8b812a1f188D80C3",
-      handleSwitch: (title, boolean) => testChangeExtension(title, boolean),
+      handleSwitch: (title, value) =>
+        testChangeExtension(title.toLowerCase(), value),
       active: activeExtensions.staking === "true" ? true : false,
+      disabled: !emailVerified,
     },
     {
       icon: <StakingIcon className={"other-extensions-card-icon"} />,
       title: "Loan",
+      value: "loan",
       description:
         "Crust pencil novel colours drift unfamed, oft line balls instructed sociis.",
       hash: "0x74a81F84268744a40FEBc48f8b812a1f188D80C3",
-      handleSwitch: (title, boolean) => testChangeExtension(title, boolean),
+      handleSwitch: (title, value) =>
+        testChangeExtension(title.toLowerCase(), value),
       active: activeExtensions.loan === "true" ? true : false,
+      disabled: !(isActive && emailVerified),
     },
     {
       icon: <StakingIcon className={"other-extensions-card-icon"} />,
       title: "Referral",
+      value: "referral",
       description:
         "Crust pencil novel colours drift unfamed, oft line balls instructed sociis.",
       hash: "0x74a81F84268744a40FEBc48f8b812a1f188D80C3",
-      handleSwitch: (title, boolean) => testChangeExtension(title, boolean),
+      handleSwitch: (title, value) =>
+        testChangeExtension(title.toLowerCase(), value),
       active: activeExtensions.referral === "true" ? true : false,
+      disabled: !emailVerified,
     },
     {
       icon: <StakingIcon className={"other-extensions-card-icon"} />,
       title: "Notifications",
+      value: "notify",
       description:
         "Crust pencil novel colours drift unfamed, oft line balls instructed sociis.",
       hash: "0x74a81F84268744a40FEBc48f8b812a1f188D80C3",
-      handleSwitch: (title, boolean) => testChangeExtension("notify", boolean),
+      handleSwitch: (title, value) => testChangeExtension("notify", value),
       active: activeExtensions.notify === "true" ? true : false,
+      disabled: false,
     },
   ];
 
