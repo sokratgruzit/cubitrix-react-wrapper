0e675e245cb12d941bddcf89b6d1ce24e3cca510
Zura Chubinashvili <chubinashvili2003@gmail.com>
2023-04-10 14:31:52 +0400

    fix: verifyEmail

--- src/components/VerifyEmail/VerifyEmail.js
+++ src/components/VerifyEmail/VerifyEmail.js
@@ -5,15 +5,17 @@ import axios from "../../api/axios";
 
 import { Logo, Verify, NotVerify } from "../../assets/svg";
 
-import { Button } from "@cubitrix/cubitrix-react-ui-module";
+import { Button, HelpText } from "@cubitrix/cubitrix-react-ui-module";
 import styles from "./VerifyEmail.module.css";
 
-const VerifyEmail = (props) => {
+const VerifyEmail = () => {
   const account = useSelector((state) => state.connect.account);
-  const params = useParams();
+  const { id } = useParams();
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [data, setData] = useState();
+  const [emailWasSent, setEmailWasSent] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const updateState = () => {
     axios
@@ -26,23 +28,52 @@ const VerifyEmail = (props) => {
           payload: res.data.success.data.accounts[0],
         });
       })
-      .catch((e) => {});
+      .catch((e) => console.log(e));
   };
 
   const verify = () => {
+    setLoading(true);
     axios
-      .post("/api/accounts/verify", { code: params.id })
+      .post("/api/accounts/verify", { code: id })
       .then((res) => {
         updateState();
         setData(JSON.parse(res.data.success));
+        setLoading(false);
       })
-      .catch((e) => setData(JSON.parse(e.response.data.success)));
+      .catch((e) => {
+        setData(JSON.parse(e.response.data.success));
+        setLoading(false);
+      });
+  };
+
+  const resendEmail = () => {
+    setLoading(true);
+    setEmailWasSent(null);
+
+    axios
+      .post("/api/accounts/resend-email", { address: account })
+      .then((res) => {
+        updateState();
+        setEmailWasSent({ success: true, message: res.data });
+        setLoading(false);
+        setTimeout(() => {
+          setEmailWasSent(null);
+        }, 3000);
+      })
+
+      .catch((e) => {
+        setEmailWasSent({ success: false, message: e?.response?.data });
+        setLoading(false);
+        setTimeout(() => {
+          setEmailWasSent(null);
+        }, 3000);
+      });
   };
 
   useEffect(() => {
     verify();
     // eslint-disable-next-line
-  }, [params]);
+  }, [id]);
 
   const handleClick = () => {
     if (data) {
@@ -53,52 +84,58 @@ const VerifyEmail = (props) => {
       navigate("/");
     }
     if (!data) {
-      setData(undefined);
-      verify();
+      resendEmail();
     }
   };
+
   return (
-    <>
-      {data === undefined ? (
+    <div className={styles.verifyEmailContainer}>
+      <div className={styles.logoContainer}>
+        <Logo />
+        <h2>COMPLEND</h2>
+      </div>
+      {loading ? (
         <div className={styles.spinnerContainer}>
           <div className={styles.spinner} />
         </div>
       ) : (
-        <div className={styles.verifyEmailContainer}>
-          <div className={styles.logoContainer}>
-            <Logo />
-            <h2>COMPLEND</h2>
+        <div className={styles.emailInfo}>
+          {data ? <Verify /> : <NotVerify />}
+          <div>
+            <h3>{data ? "Verify your Email" : "Email Not Verify"}</h3>
+            <p>
+              {data ? (
+                "Your email address has been verified. Now you can set up security"
+              ) : (
+                <>
+                  <span>Your email address hasn’t been verified.</span>
+                  <span>
+                    Please try again or click on the reset button to receive a
+                    new verification mail.
+                  </span>
+                </>
+              )}
+            </p>
           </div>
-          <div className={styles.emailInfo}>
-            {data ? <Verify /> : <NotVerify />}
-            <div>
-              <h3>{data ? "Verify your Email" : "Email Not Verify"}</h3>
-              <p>
-                {data ? (
-                  "Your email address has been verified. Now you can set up security"
-                ) : (
-                  <>
-                    <span>Your email address hasn’t been verified.</span>
-                    <span>
-                      Please try again or click on the reset button to receive a new
-                      verification mail.
-                    </span>
-                  </>
-                )}
-              </p>
-            </div>
-            <Button
-              element="button"
-              label={data ? "Set Up Security" : "Resend Verification"}
-              onClick={handleClick}
-              type="btn-primary"
-              size="btn-sm"
-              customStyles={{ background: '#00C6FF'}}
+          <Button
+            element='button'
+            label={data ? "Set Up Security" : "Resend Verification"}
+            onClick={handleClick}
+            type='btn-primary'
+            size='btn-sm'
+            customStyles={{ background: "#00C6FF" }}
+          />
+          {emailWasSent && (
+            <HelpText
+              status={emailWasSent.success ? "success" : "error"}
+              title={emailWasSent.message}
+              fontSize={"font-12"}
+              icon={true}
             />
-          </div>
+          )}
         </div>
       )}
-    </>
+    </div>
   );
 };
 
