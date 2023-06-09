eb3158242843a07a3247f6565e5005dc46dc191b
Zura Chubinashvili <chubinashvili2003@gmail.com>
2023-06-09 14:38:41 +0400

    last

--- src/components/LandingRegistration.js
+++ src/components/LandingRegistration.js
@@ -1,70 +1,67 @@
-import { LandingSteps } from "@cubitrix/cubitrix-react-ui-module";
-import React, { useState, useEffect } from "react";
+import { LandingSteps } from '@cubitrix/cubitrix-react-ui-module'
+import React, { useState, useEffect } from 'react'
 
-import { useConnect, useStake } from "@cubitrix/cubitrix-react-connect-module";
-import { injected, walletConnect } from "../connector";
+import { useConnect, useStake } from '@cubitrix/cubitrix-react-connect-module'
+import { injected, walletConnect } from '../connector'
 
-import { useSelector, useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
-import { useTableParameters } from "../hooks/useTableParameters";
+import { useTableParameters } from '../hooks/useTableParameters'
 
-import axios from "../api/axios";
-import QRCode from "qrcode";
-import WBNB from "../abi/WBNB.json";
+import axios from '../api/axios'
+import QRCode from 'qrcode'
+import WBNB from '../abi/WBNB.json'
 
 const LandingRegistration = ({ step, setStep, setInitialRegister }) => {
-  const account = useSelector((state) => state.connect.account);
-  const triedReconnect = useSelector((state) => state.appState?.triedReconnect);
-  const appState = useSelector((state) => state.appState);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const account = useSelector(state => state.connect.account)
+  const triedReconnect = useSelector(state => state.appState?.triedReconnect)
+  const appState = useSelector(state => state.appState)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-  const { connect, disconnect, error, setError, connectionLoading, library } =
-    useConnect();
+  const { connect, disconnect, error, setError, connectionLoading, library } = useConnect()
 
   // const [loading, setLoading] = useState(true);
 
-  const [qrCodeUrl, setQrCodeUrl] = useState("");
-  const [hostedUrl, setHostedUrl] = useState("");
+  const [qrCodeUrl, setQrCodeUrl] = useState('')
+  const [hostedUrl, setHostedUrl] = useState('')
 
-  const [receivePaymentAddress, setReceivePaymentAddress] = useState(
-    "0x43f59F41518903A274c7897dfFB24DB86a0dd23a",
-  );
+  const [receivePaymentAddress, setReceivePaymentAddress] = useState('0x43f59F41518903A274c7897dfFB24DB86a0dd23a')
 
   useEffect(() => {
     if (receivePaymentAddress) {
       QRCode.toDataURL(receivePaymentAddress)
-        .then((url) => {
-          setQrCodeUrl(url);
+        .then(url => {
+          setQrCodeUrl(url)
         })
-        .catch((err) => {
-          console.error(err);
-        });
+        .catch(err => {
+          console.error(err)
+        })
     }
-  }, [receivePaymentAddress]);
+  }, [receivePaymentAddress])
 
   useEffect(() => {
     if (hostedUrl) {
-      window.location.href = hostedUrl;
+      window.location.href = hostedUrl
     }
-  }, [hostedUrl]);
+  }, [hostedUrl])
 
   async function handlePaymentConfirm(userAddress, selectedMethod, amount, date) {
     axios
-      .post("api/transactions/pending_deposit_transaction", {
+      .post('api/transactions/pending_deposit_transaction', {
         from: account,
         amount: amount,
         amountTransferedFrom: userAddress,
         receivePaymentAddress: receivePaymentAddress,
         startDate: date,
       })
-      .then((res) => {
-        console.log(res);
+      .then(res => {
+        console.log(res)
       })
-      .catch((err) => {
-        console.error(err);
-      });
+      .catch(err => {
+        console.error(err)
+      })
   }
 
   const methods = [
@@ -74,11 +71,11 @@ const LandingRegistration = ({ step, setStep, setInitialRegister }) => {
     //   logo: "https://shopgeorgia.ge/assets/images/pay-manual.png",
     // },
     {
-      id: "Coinbase",
-      title: "Coinbase",
-      logo: "https://shopgeorgia.ge/assets/images/contribute/eth.png",
+      id: 'Coinbase',
+      title: 'Coinbase',
+      logo: 'https://shopgeorgia.ge/assets/images/contribute/eth.png',
     },
-  ];
+  ]
 
   const paymentTypes = [
     // {
@@ -88,152 +85,152 @@ const LandingRegistration = ({ step, setStep, setInitialRegister }) => {
     // },
     {
       id: 2,
-      title: "Pay with CoinBase",
-      logo: "https://shopgeorgia.ge/assets/images/contribute/eth.png",
+      title: 'Pay with CoinBase',
+      logo: 'https://shopgeorgia.ge/assets/images/contribute/eth.png',
     },
-  ];
+  ]
 
   const [registrationState, setRegistrationState] = useState({
     loading: false,
-    fullnameError: "",
-    emailError: "",
-    referralError: "",
+    fullnameError: '',
+    emailError: '',
+    referralError: '',
     emailSent: false,
-  });
+  })
 
   async function handleRegistration({ fullName, email, referral }) {
-    const errors = {};
+    const errors = {}
     if (!fullName) {
-      errors.fullNameError = "Full Name is required";
+      errors.fullNameError = 'Full Name is required'
     }
-    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
     if (email && !emailRegex.test(email)) {
-      errors.emailError = "Invalid email";
+      errors.emailError = 'Invalid email'
     }
     if (!email) {
-      errors.emailError = "Email is required";
+      errors.emailError = 'Email is required'
     }
     if (!referral) {
-      errors.referralError = "Referral code is required";
+      errors.referralError = 'Referral code is required'
     }
 
     if (Object.keys(errors).length > 0) {
       setRegistrationState({
         ...registrationState,
         ...errors,
-      });
-      return;
+      })
+      return
     }
 
     setRegistrationState({
       ...registrationState,
       loading: true,
-    });
+    })
 
     axios
-      .post("api/referral/assign_refferal_to_user", {
+      .post('api/referral/assign_refferal_to_user', {
         referral,
         address: account,
       })
-      .then((res) => {
-        update_profile();
+      .then(res => {
+        update_profile()
       })
-      .catch((err) => {
-        let error = "Referral code could not be assigned";
-        if (err?.response?.data === "Referral code doesnot exist") {
-          error = "Referral code does not exist";
+      .catch(err => {
+        let error = 'Referral code could not be assigned'
+        if (err?.response?.data === 'Referral code doesnot exist') {
+          error = 'Referral code does not exist'
         }
         let errorsMessages = [
-          "User already activated both referral code",
-          "User already activated uni level referral code",
-          "User already activated binary level referral code",
-        ];
+          'User already activated both referral code',
+          'User already activated uni level referral code',
+          'User already activated binary level referral code',
+        ]
         if (errorsMessages.includes(err?.response?.data)) {
-          update_profile();
-          return;
+          update_profile()
+          return
         }
 
         setRegistrationState({
           ...registrationState,
           referralError: error,
           loading: false,
-        });
-      });
+        })
+      })
 
     async function update_profile() {
       axios
-        .post("/api/accounts/update_profile", {
+        .post('/api/accounts/update_profile', {
           address: account,
           name: fullName,
           email,
         })
-        .then((res) => {
-          if (res?.data === "email sent") {
-            setRegistrationState((prev) => ({
+        .then(res => {
+          if (res?.data === 'email sent') {
+            setRegistrationState(prev => ({
               ...prev,
               emailSent: true,
               loading: false,
-            }));
+            }))
           }
-          if (res?.data === "account updated") {
-            getBalance().then((balance) => {
-              let step = 3;
+          if (res?.data === 'account updated') {
+            getBalance().then(balance => {
+              let step = 3
               if (balance > 100) {
-                step = 4;
+                step = 4
               }
               // don't forget
 
               axios
-                .post("/api/accounts/handle-step", { step, address: account })
-                .then((e) => {
-                  setStep(e?.data?.account?.step ?? 3);
+                .post('/api/accounts/handle-step', { step, address: account })
+                .then(e => {
+                  setStep(e?.data?.account?.step ?? 3)
                   setRegistrationState({
                     ...registrationState,
-                    referralError: "something went wrong!",
+                    referralError: 'something went wrong!',
                     loading: false,
-                  });
+                  })
                 })
-                .catch((e) => {
+                .catch(e => {
                   setRegistrationState({
                     ...registrationState,
-                    referralError: "something went wrong!",
+                    referralError: 'something went wrong!',
                     loading: false,
-                  });
-                });
-            });
+                  })
+                })
+            })
           }
         })
-        .catch((err) => {
-          if (err?.response?.data === "email already exists & is verified") {
-            setRegistrationState((prev) => ({
+        .catch(err => {
+          if (err?.response?.data === 'email already exists & is verified') {
+            setRegistrationState(prev => ({
               ...prev,
-              emailError: "Email is already in use.",
+              emailError: 'Email is already in use.',
               loading: false,
-            }));
+            }))
           }
 
           setTimeout(() => {
             setRegistrationState({
               ...registrationState,
-              emailError: "",
-            });
-          }, 3000);
-        });
+              emailError: '',
+            })
+          }, 3000)
+        })
     }
   }
 
   async function getBalance() {
-    var tokenContract = new library.eth.Contract(WBNB, tokenAddress);
-    var decimals = await tokenContract.methods.decimals().call();
-    var getBalance = await tokenContract.methods.balanceOf(account).call();
+    var tokenContract = new library.eth.Contract(WBNB, tokenAddress)
+    var decimals = await tokenContract.methods.decimals().call()
+    var getBalance = await tokenContract.methods.balanceOf(account).call()
 
-    var pow = 10 ** decimals;
-    var balanceInEth = getBalance / pow;
+    var pow = 10 ** decimals
+    var balanceInEth = getBalance / pow
 
-    return balanceInEth;
+    return balanceInEth
   }
 
-  const metaAcc = appState?.userData?.meta;
+  const metaAcc = appState?.userData?.meta
   // useEffect(() => {
   //   if (account && triedReconnect) {
   //     if (metaAcc && metaAcc.email && metaAcc.name) {
@@ -260,171 +257,168 @@ const LandingRegistration = ({ step, setStep, setInitialRegister }) => {
   // }, [account]);
 
   const [formData, setFormData] = useState({
-    fullName: "",
-    email: "",
-    referral: "",
-  });
+    fullName: '',
+    email: '',
+    referral: '',
+  })
 
   useEffect(() => {
     if (appState?.userData) {
       setFormData({
-        fullName: appState?.userData?.meta?.name ?? "",
-        email: appState?.userData?.meta?.email ?? "",
-        referral: appState?.userData?.referral?.[0]?.referral ?? "",
-      });
+        fullName: appState?.userData?.meta?.name ?? '',
+        email: appState?.userData?.meta?.email ?? '',
+        referral: appState?.userData?.referral?.[0]?.referral ?? '',
+      })
     }
-  }, [appState?.userData]);
+  }, [appState?.userData])
 
   async function resendEmail() {
     axios
-      .post("/api/accounts/resend-email", {
+      .post('/api/accounts/resend-email', {
         address: account,
       })
-      .then((res) => {
-        console.log(res.response);
+      .then(res => {
+        console.log(res.response)
       })
-      .catch((e) => {
-        console.log(e.response);
-      });
+      .catch(e => {
+        console.log(e.response)
+      })
   }
 
   async function handleCoindbasePayment(amount) {
     axios
-      .post("api/transactions/coinbase_deposit_transaction", {
+      .post('api/transactions/coinbase_deposit_transaction', {
         from: account,
         amount,
       })
-      .then((res) => {
-        setHostedUrl(res?.data?.responseData?.hosted_url);
+      .then(res => {
+        setHostedUrl(res?.data?.responseData?.hosted_url)
       })
-      .catch((err) => {
-        console.error(err);
-      });
+      .catch(err => {
+        console.error(err)
+      })
   }
 
   async function handlePurchaseEvent(method, amount) {
-    if (method === "Coinbase") {
-      handleCoindbasePayment(amount);
+    if (method === 'Coinbase') {
+      handleCoindbasePayment(amount)
     }
   }
 
   const [currentObject, setCurrentObject] = useState({
-    amount: "",
-  });
+    amount: '',
+  })
 
-  const { durationOptions } = useTableParameters("staking");
+  const { durationOptions } = useTableParameters('staking')
 
-  var tokenAddress = "0xE807fbeB6A088a7aF862A2dCbA1d64fE0d9820Cb"; // Staking Token Address
-  var Router = "0xd472C9aFa90046d42c00586265A3F62745c927c0"; // Staking contract Address
-  const { approve, stake, handleTimeperiodDate, handleDepositAmount, handleTimePeriod } =
-    useStake({
-      Router,
-      tokenAddress,
-    });
+  var tokenAddress = '0xE807fbeB6A088a7aF862A2dCbA1d64fE0d9820Cb' // Staking Token Address
+  var Router = '0xd472C9aFa90046d42c00586265A3F62745c927c0' // Staking contract Address
+  const { approve, stake, handleTimeperiodDate, handleDepositAmount, handleTimePeriod } = useStake({
+    Router,
+    tokenAddress,
+  })
 
-  const { depositAmount, timeperiod, isAllowance, timeperiodDate, loading } = useSelector(
-    (state) => state.stake,
-  );
+  const { depositAmount, timeperiod, isAllowance, timeperiodDate, loading } = useSelector(state => state.stake)
 
   const inputs = [
     {
-      title: "Amount",
-      name: "amount",
-      type: "default",
-      placeholder: "0",
-      onChange: (e) => {
-        handleDepositAmount(e.target.value);
-        setCurrentObject((prev) => ({
+      title: 'Amount',
+      name: 'amount',
+      type: 'default',
+      placeholder: '0',
+      onChange: e => {
+        handleDepositAmount(e.target.value)
+        setCurrentObject(prev => ({
           ...prev,
           [e.target.name]: e.target.value,
-        }));
+        }))
       },
     },
-  ];
+  ]
 
-  const [stakeButtonText, setStakeButtonText] = useState("Approve");
+  const [stakeButtonText, setStakeButtonText] = useState('Approve')
 
   useEffect(() => {
     if (loading) {
-      setStakeButtonText("Loading...");
+      setStakeButtonText('Loading...')
     } else if (account && isAllowance) {
-      setStakeButtonText("Approve");
+      setStakeButtonText('Approve')
     } else {
-      setStakeButtonText("Stake");
+      setStakeButtonText('Stake')
     }
-  }, [account, isAllowance, loading]);
+  }, [account, isAllowance, loading])
 
   const handleDepositSubmit = async () => {
-    if (depositAmount < 1 && currentObject?.amount === "0") {
+    if (depositAmount < 1 && currentObject?.amount === '0') {
     }
 
     if (account && isAllowance) {
-      approve(() => {});
+      approve(() => {})
     }
     if (account && !isAllowance) {
       stake(async () => {
-        setStep(5);
+        setStep(5)
         axios
-          .post("/api/accounts/handle-step", {
+          .post('/api/accounts/handle-step', {
             registered: true,
             address: account,
             step: 5,
           })
-          .then((res) => {
+          .then(res => {
             dispatch({
-              type: "UPDATE_ACTIVE_EXTENSIONS",
-              payload: { dashboard: "true" },
-            });
+              type: 'UPDATE_ACTIVE_EXTENSIONS',
+              payload: { dashboard: 'true' },
+            })
           })
-          .catch((err) => {
-            console.log(err);
-          });
-        navigate("/dashboard");
+          .catch(err => {
+            console.log(err)
+          })
+        navigate('/dashboard')
         axios
-          .post("/api/accounts/manage_extensions", {
+          .post('/api/accounts/manage_extensions', {
             address: account,
-            extensions: { staking: "true", referral: "true" },
+            extensions: { staking: 'true', referral: 'true' },
           })
-          .then((res) => {
+          .then(res => {
             if (res?.data?.account) {
-              console.log("res", res?.data);
+              console.log('res', res?.data)
               dispatch({
-                type: "UPDATE_ACTIVE_EXTENSIONS",
+                type: 'UPDATE_ACTIVE_EXTENSIONS',
                 payload: res.data.account.extensions,
-              });
+              })
             }
           })
-          .catch((e) => console.log(e.response));
+          .catch(e => console.log(e.response))
         await axios
-          .post("/api/accounts/activate-account", {
+          .post('/api/accounts/activate-account', {
             address: account,
           })
-          .then((res) => {
+          .then(res => {
             if (res.data?.account) {
               dispatch({
-                type: "SET_SYSTEM_ACCOUNT_DATA",
+                type: 'SET_SYSTEM_ACCOUNT_DATA',
                 payload: res.data.account,
-              });
+              })
               setTimeout(() => {
-                setCurrentObject((prev) => ({ ...prev, amount: "0" }));
-                handleDepositAmount(0);
-              }, 3000);
+                setCurrentObject(prev => ({ ...prev, amount: '0' }))
+                handleDepositAmount(0)
+              }, 3000)
             }
           })
-          .catch((e) => {});
-      });
+          .catch(e => {})
+      })
     }
-  };
+  }
 
   return (
     <LandingSteps
       account={account}
       receivePaymentAddress={receivePaymentAddress}
       handleMetamaskConnect={async () => {
-        await connect("metaMask", injected);
+        await connect('metaMask', injected)
       }}
       handleWalletConnect={async () => {
-        await connect("walletConnect", walletConnect);
+        await connect('walletConnect', walletConnect)
       }}
       connectionLoading={connectionLoading}
       step={step}
@@ -436,7 +430,7 @@ const LandingRegistration = ({ step, setStep, setInitialRegister }) => {
       registrationState={registrationState}
       setRegistrationState={setRegistrationState}
       handlePaymentConfirm={handlePaymentConfirm}
-      handleCoindbasePayment={(amount) => handleCoindbasePayment(amount)}
+      handleCoindbasePayment={amount => handleCoindbasePayment(amount)}
       formData={formData}
       setFormData={setFormData}
       resendEmail={resendEmail}
@@ -456,7 +450,7 @@ const LandingRegistration = ({ step, setStep, setInitialRegister }) => {
       inputs={inputs}
       currentObject={currentObject}
     />
-  );
-};
+  )
+}
 
-export default LandingRegistration;
+export default LandingRegistration
