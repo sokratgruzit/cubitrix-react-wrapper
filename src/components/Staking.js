cab0b3b11ad2f4ec2562e795179c1bf675ae0525
Zura Chubinashvili <chubinashvili2003@gmail.com>
2023-06-12 16:16:46 +0400

    update

--- src/components/Staking.js
+++ src/components/Staking.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 import {
   ClaimedReward,
@@ -9,32 +9,29 @@ import {
   TotalUnstaked,
   WalletBalance,
   AddSquareIcon,
-} from "../assets/svg";
+} from '../assets/svg'
 
-// import { useStake } from "@cubitrix/cubitrix-react-connect-module";
-import { useStake } from "../hooks/use-stake";
+import { useStake, STAKE_INIT_STATE, useConnect } from '@cubitrix/cubitrix-react-connect-module'
 
 // hooks
-import { useTableParameters } from "../hooks/useTableParameters";
+import { useTableParameters } from '../hooks/useTableParameters'
 
 // UI
-import {
-  Staking as StakingUI,
-  Button,
-  Popup,
-  Calculator,
-} from "@cubitrix/cubitrix-react-ui-module";
+import { Staking as StakingUI, Button, Popup, Calculator } from '@cubitrix/cubitrix-react-ui-module'
 
 // api
-import axios from "../api/axios";
+import axios from '../api/axios'
+import { useEffect } from 'react'
 
 const Staking = () => {
-  const [createStakingPopUpActive, setCreateStakingPopUpActive] = useState(false);
-  const [approveResonse, setApproveResonse] = useState(null);
+  const [createStakingPopUpActive, setCreateStakingPopUpActive] = useState(false)
+  const [approveResonse, setApproveResonse] = useState(null)
+  const triedReconnect = useSelector(state => state.appState?.triedReconnect)
+  const { active, account } = useConnect()
 
-  const sideBarOpen = useSelector((state) => state.appState.sideBarOpen);
-  var Router = "0xd472C9aFa90046d42c00586265A3F62745c927c0"; // Staking contract Address
-  var tokenAddress = "0xE807fbeB6A088a7aF862A2dCbA1d64fE0d9820Cb"; // Staking Token Address
+  const sideBarOpen = useSelector(state => state.appState.sideBarOpen)
+  var Router = '0xd472C9aFa90046d42c00586265A3F62745c927c0' // Staking contract Address
+  var tokenAddress = '0xE807fbeB6A088a7aF862A2dCbA1d64fE0d9820Cb' // Staking Token Address
   const {
     approve,
     stake,
@@ -44,10 +41,11 @@ const Staking = () => {
     handleTimeperiodDate,
     handleDepositAmount,
     handleTimePeriod,
-    account,
-  } = useStake({ Router, tokenAddress });
+    getStackerInfo,
+    checkAllowance,
+  } = useStake({ Router, tokenAddress })
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   const {
     depositAmount,
@@ -59,158 +57,180 @@ const Staking = () => {
     isAllowance,
     loading,
     timeperiodDate,
-  } = useSelector((state) => state.stake);
+  } = useSelector(state => state.stake)
+
+  useEffect(() => {
+    if (account && triedReconnect && active) {
+      checkAllowance()
+      getStackerInfo()
+    }
+    // eslint-disable-next-line
+  }, [account, triedReconnect, active, depositAmount])
+
+  useEffect(() => {
+    if (!account && triedReconnect && !active) {
+      dispatch({
+        type: 'UPDATE_STAKE_STATE',
+        payload: {
+          ...STAKE_INIT_STATE,
+        },
+      })
+    }
+    // eslint-disable-next-line
+  }, [account, triedReconnect, active, depositAmount])
+
+  console.log(stakersRecord)
 
   const handleConnect = () => {
     if (sideBarOpen) {
       dispatch({
-        type: "SET_SIDE_BAR",
+        type: 'SET_SIDE_BAR',
         payload: { sideBarOpen: !sideBarOpen },
-      });
+      })
     } else {
       dispatch({
-        type: "SET_SIDE_BAR",
-        payload: { sideBarOpen: !sideBarOpen, sideBar: "connect" },
-      });
+        type: 'SET_SIDE_BAR',
+        payload: { sideBarOpen: !sideBarOpen, sideBar: 'connect' },
+      })
     }
-  };
+  }
 
   const handlePopUpOpen = () => {
-    setCreateStakingPopUpActive(true);
-  };
+    setCreateStakingPopUpActive(true)
+  }
 
   const th = [
     {
-      name: "Staked Amount",
+      name: 'Staked Amount',
       width: 15,
       mobileWidth: 45,
       id: 0,
     },
     {
-      name: "Stake Date ",
+      name: 'Stake Date ',
       width: 15,
       id: 1,
     },
     {
-      name: "Unstake Date",
+      name: 'Unstake Date',
       width: 15,
       id: 2,
     },
     {
-      name: "Earn Reward",
+      name: 'Earn Reward',
       width: 15,
       id: 3,
     },
     {
-      name: "Harvest",
+      name: 'Harvest',
       width: 15,
       mobileWidth: 45,
       id: 4,
     },
     {
-      name: "",
+      name: '',
       width: 10,
       id: 5,
       mobileWidth: 35,
-      position: "right",
-      className: "buttons-th",
-      onClick: (index) => unstake(index),
+      position: 'right',
+      className: 'buttons-th',
+      onClick: index => unstake(index),
     },
     {
-      name: "",
+      name: '',
       width: 7,
       id: 6,
       mobileWidth: 20,
-      position: "right",
-      className: "buttons-th",
-      onClick: (index) => harvest(index),
+      position: 'right',
+      className: 'buttons-th',
+      onClick: index => harvest(index),
     },
-  ];
+  ]
 
-  const { durationOptions } = useTableParameters("staking");
+  const { durationOptions } = useTableParameters('staking')
 
   const accountSummaryData = [
     [
       {
         icon: <CurrentStake />,
-        title: "Current Stake",
+        title: 'Current Stake',
         value: parseFloat(stakersInfo.currentStaked).toFixed(5),
       },
       {
         icon: <Earn />,
-        title: "Earn",
+        title: 'Earn',
         value: parseFloat(stakersInfo.realtimeReward).toFixed(10),
       },
       {
         icon: <ClaimedReward />,
-        title: "Claimed Reward",
+        title: 'Claimed Reward',
         value: parseFloat(stakersInfo.totalClaimedRewardTokenUser).toFixed(5),
       },
     ],
     [
       {
         icon: <WalletBalance />,
-        title: "Your Wallet Balance",
+        title: 'Your Wallet Balance',
         value: balance.toFixed(5),
       },
       {
         icon: <TotalStaked />,
-        title: "Total Staked",
+        title: 'Total Staked',
         value: parseFloat(stakersInfo.totalStakedTokenUser).toFixed(5),
       },
       {
         icon: <TotalUnstaked />,
-        title: "Total Unstaked",
+        title: 'Total Unstaked',
         value: parseFloat(stakersInfo.totalUnstakedTokenUser).toFixed(5),
       },
     ],
-  ];
+  ]
 
   const handleCalculatorSubmit = async () => {
-    setApproveResonse(null);
+    setApproveResonse(null)
     if (!account) {
-      handleConnect();
+      handleConnect()
     }
 
     if (account && isAllowance) {
       approve(() => {
         setApproveResonse({
-          status: "success",
-          message: "Approved successfully, please stake desired amount.",
-        });
-      });
+          status: 'success',
+          message: 'Approved successfully, please stake desired amount.',
+        })
+      })
     }
     if (account && !isAllowance) {
       stake(async () => {
         await axios
-          .post("/api/accounts/activate-account", {
+          .post('/api/accounts/activate-account', {
             address: account,
           })
-          .then((res) => {
+          .then(res => {
             if (res.data?.account) {
               dispatch({
-                type: "SET_SYSTEM_ACCOUNT_DATA",
+                type: 'SET_SYSTEM_ACCOUNT_DATA',
                 payload: res.data.account,
-              });
+              })
             }
           })
-          .catch((e) => {});
-        setCreateStakingPopUpActive(false);
-      });
+          .catch(e => {})
+        setCreateStakingPopUpActive(false)
+      })
     }
-  };
+  }
 
   const tableEmptyData = {
-    label: "Stake to earn Complend reward",
+    label: 'Stake to earn Complend reward',
     button: (
       <Button
-        element={"referral-button"}
-        label={"Create Staking"}
+        element={'referral-button'}
+        label={'Create Staking'}
         icon={<AddSquareIcon color={`#00C6FF`} />}
         onClick={handlePopUpOpen}
       />
     ),
-  };
+  }
 
   return (
     <>
@@ -245,14 +265,14 @@ const Staking = () => {
               approveResonse={approveResonse}
             />
           }
-          label={"Staking Calculator"}
+          label={'Staking Calculator'}
           handlePopUpClose={() => setCreateStakingPopUpActive(false)}
-          description={"Stake Complend to earn Complend reward"}
-          headerCustomStyles={{ background: "#272C57" }}
+          description={'Stake Complend to earn Complend reward'}
+          headerCustomStyles={{ background: '#272C57' }}
         />
       )}
     </>
-  );
-};
+  )
+}
 
-export default Staking;
+export default Staking
