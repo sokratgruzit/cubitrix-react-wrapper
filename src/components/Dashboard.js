2a16a8bb53987535fcb6ad94f47306af52978eeb
Zura Chubinashvili <chubinashvili2003@gmail.com>
2023-06-27 12:56:18 +0400

    last

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -144,6 +144,7 @@ const Dashboard = () => {
         type: "SET_ACCOUNTS_DATA",
         payload: data?.data,
       });
+      console.log("data", data?.data)
     } catch (error) {
       console.error("Error:", error);
     }
