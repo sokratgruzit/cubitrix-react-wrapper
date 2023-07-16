b0007ed7f4ef4bde98d2d3bc941f5273e1a675f7
gogaokradze <deathofsuper@gmail.com>
2023-07-16 18:25:33 +0400

    fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "https://cubitrix-idq6.onrender.com";
+// const BASE_URL = "https://cubitrix-idq6.onrender.com";
+const BASE_URL = "https://cubitrix-node-server.onrender.com";
 // const BASE_URL = "http://localhost:4000";
 
 axios.defaults.baseURL = BASE_URL;
