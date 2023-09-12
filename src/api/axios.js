04abe0a7a80165c8e628bafa34755cb5f592d5aa
gogaokradze <deathofsuper@gmail.com>
2023-09-12 12:54:36 +0400

    fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const BASE_URL = "https://cubitrix-node-server.onrender.com";
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = process.env.REACT_APP_BACKEND_URL;
 
 const instance = axios.create({
   baseURL: BASE_URL,
